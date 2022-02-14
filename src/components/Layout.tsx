0b17a540f76e17dcdb426631d7fff1e12944dfaa
elinarnten <elin.arnten@hotmail.com>
2022-02-14 09:54:40 +0100

    first commit

---
+++ src/components/Layout.tsx
@@ -0,0 +1,14 @@
+import Header from "./Header";
+import Main from "./Main";
+import "./layout.css";
+
+function Layout() {
+  return (
+    <div>
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default Layout;
---
+++ src/components/Main.tsx
@@ -0,0 +1,15 @@
+import "./main.css";
+function Main() {
+  return (
+    <main>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam id
+        repellat ipsa autem mollitia ea animi debitis deserunt dolores vitae
+        dignissimos aliquid inventore, dolore molestias corporis. Consequuntur
+        corporis quia et?
+      </p>
+    </main>
+  );
+}
+
+export default Main;
